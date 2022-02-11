915c9443bc38c5f39fe1c02ab738ad02574415ed
gulduomer <43722346+omerfguldu@users.noreply.github.com>
2022-02-11 12:08:24 +0300

    form submit transactions final commit

--- javascript/formevents/formevents.js
+++ javascript/formevents/formevents.js
@@ -1,7 +1,15 @@
 let userForm = document.querySelector("#form-user-info");
 userForm.addEventListener('submit', submitFunction);
 
+let nameInput = document.querySelector("#fname");
+let surnameInput = document.querySelector("#lname");
+let telInput = document.querySelector("#telno");
+let addressInput = document.querySelector("#address");
+
 function submitFunction(e){
     e.preventDefault();
-    console.log("işlem");
+    console.log(nameInput.value);
+    console.log(surnameInput.value);
+    console.log(telInput.value);
+    console.log(addressInput.value);
 }
