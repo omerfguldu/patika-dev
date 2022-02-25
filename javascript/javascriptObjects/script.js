3f484be67e9befc2167492b243ad8a1491745d22
gulduomer <43722346+omerfguldu@users.noreply.github.com>
2022-02-25 19:03:07 +0300

    javascript object example

--- javascript/javascriptObjects/script.js
+++ javascript/javascriptObjects/script.js
@@ -3,3 +3,40 @@ let obj1 = { a: 'Omer', b: 'Cafer', c: 'Meto' }
 Object.values(obj1).forEach(item => {
     console.log(item)
 })
+
+console.log(obj1);
+
+let countriesTalkSpanish = {
+    continent: 'South America',
+    language: 'Spanish'
+};
+
+let uruguay = Object.create(countriesTalkSpanish);
+uruguay.capital = 'Montevido'
+
+
+console.log(uruguay)
+console.log(obj1.a)
+
+obj1.d = 'Faruk'
+console.log(obj1.d);
+obj1.d = 'Faruk Güldü'
+console.log(obj1.d);
+console.log(obj1);
+delete obj1.d
+console.log(obj1);
+console.log(Object.entries(obj1));
+
+Object.keys(obj1).forEach(item => {
+    console.log(item);
+    console.log(obj1[item]);
+});
+
+function objectKeyValues(objectName) {
+    Object.entries(objectName).forEach(item => {
+        console.log(item);
+    })
+}
+
+objectKeyValues(obj1);
+console.log(objectKeyValues["name"]);
