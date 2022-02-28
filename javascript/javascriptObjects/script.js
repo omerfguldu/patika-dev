80e2539dfc968634fc815fcfc138a9f584983ce4
gulduomer <43722346+omerfguldu@users.noreply.github.com>
2022-02-28 10:10:05 +0300

    object destructring exapmles added

--- javascript/javascriptObjects/script.js
+++ javascript/javascriptObjects/script.js
@@ -40,3 +40,38 @@ function objectKeyValues(objectName) {
 
 objectKeyValues(obj1);
 console.log(objectKeyValues["name"]);
+
+
+const cikarma = ({ sayi1, sayi2 }) => {
+    return console.log(sayi1 - sayi2);
+}
+
+const sayilar = {
+    sayi2: 3,
+    sayi1: 5,
+}
+cikarma(sayilar)
+
+
+const toplama = ({ s1, s2, ...args }) => {
+    toplam = 0
+    toplam = s1 + s2;
+    for (const key in args) {
+        toplam += args[key]
+    }
+    console.log(toplam);
+}
+
+const toplanacakSayilar = {
+    s1: 8,
+    s2: 6,
+    s3: 4,
+    s4: 2,
+    s5: 0,
+    s6: 7,
+    s7: 5,
+    s8: 3,
+    s9: 1
+}
+
+toplama(toplanacakSayilar);
