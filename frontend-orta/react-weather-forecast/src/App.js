9a0015ab1fbaaa5656edef71e8980410c47ac288
gulduomer <43722346+omerfguldu@users.noreply.github.com>
2022-08-25 12:36:59 +0300

    arka plan gorseli eklendi ve gradient efekti uygulandi. genel ve ayrintili bilgi kisimlarinin htmlleri olusturuldu. genel bilgi kismi icin stillendirme islemleri tamamlandi.

--- frontend-orta/react-weather-forecast/src/App.js
+++ frontend-orta/react-weather-forecast/src/App.js
@@ -1,11 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import img from "./assets/weather-forecast-bg.jpg";
+import "./App.css";
+import GeneralData from "./components/GeneralData";
+import DetailedData from "./components/DetailedData";
 
 function App() {
   return (
-    <div >
-      
-    </div>
+    <>
+      <div className="bg-effect">
+        {/* <img src={img} className="bg-image"></img> */}
+      </div>
+      <div className="container">
+        <div className="container-data">
+          <GeneralData />
+          <DetailedData />
+        </div>
+      </div>
+    </>
   );
 }
 
---
+++ frontend-orta/react-weather-forecast/src/components/DetailedData.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+function DetailedData() {
+  return (
+    <div className="container-detailed">
+      <div className="container-detailed-info">
+        <img src="" alt="" />
+        <div className="detailed-info">
+          <p>Hissedilen Sicaklik</p>
+          <p className="detailed-text">32 C</p>
+        </div>
+      </div>
+      <div className="container-detailed-info">
+        <img src="" alt="" />
+        <div className="detailed-info">
+          <p>Hissedilen Sicaklik</p>
+          <p className="detailed-text">32 C</p>
+        </div>
+      </div>
+      <div className="container-detailed-info">
+        <img src="" alt="" />
+        <div className="detailed-info">
+          <p>Hissedilen Sicaklik</p>
+          <p className="detailed-text">32 C</p>
+        </div>
+      </div>
+      <div className="container-detailed-info">
+        <img src="" alt="" />
+        <div className="detailed-info">
+          <p>Hissedilen Sicaklik</p>
+          <p className="detailed-text">32 C</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default DetailedData;
